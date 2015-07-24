75144e181b8ab48817c7a4f979644cbc5d0ffd4f
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2015-07-23 22:32:49 -0300

    Bug correction.

--- src/main/java/cmabreu/sagitarii/action/NodeSSHTerminalAction.java
+++ src/main/java/cmabreu/sagitarii/action/NodeSSHTerminalAction.java
@@ -47,11 +47,7 @@ public class NodeSSHTerminalAction extends BasicActionClass {
 				
 				if ( (user != null) && (password != null)  ) {
 					String host = cluster.getIpAddress();
-					//session = mngr.newSession( cluster.getMacAddress(), host, port, user, password );
-					
-					// session = mngr.newSession( cluster.getMacAddress(), "eic.cefet-rj.br", 8091, "sagitarii", "Chiron2014!" );
-					session = mngr.newSession( cluster.getMacAddress(), "10.5.112.214", 22, "sadlog", "sadlog" );
-					
+					session = mngr.newSession( cluster.getMacAddress(), host, port, user, password );
 				} else {
 					setMessageText("user and password must be set");						
 				}
