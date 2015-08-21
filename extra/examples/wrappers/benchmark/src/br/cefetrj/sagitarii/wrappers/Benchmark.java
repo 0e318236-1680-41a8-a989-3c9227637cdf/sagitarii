6a547ce389555f2c9bc3210ed2f3ea3cca54a1af
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2015-08-21 14:35:46 -0300

    Created Benchmark wrapper

--- extra/examples/wrappers/benchmark/src/br/cefetrj/sagitarii/wrappers/Benchmark.java
+++ extra/examples/wrappers/benchmark/src/br/cefetrj/sagitarii/wrappers/Benchmark.java
@@ -3,14 +3,22 @@ package br.cefetrj.sagitarii.wrappers;
 import cmabreu.sagitarii.sdk.Wrapper;
 
 
-public class Main {
+public class Benchmark {
 	
 	public static void main(String[] args) throws Exception{
 		Processor myProcessor = new Processor();
 		try {
-			Wrapper wrapper = new Wrapper("Echo Wrapper", args[0], args[1], myProcessor );
+			Wrapper wrapper = new Wrapper("Benchmark", args[0], args[1], myProcessor );
 			wrapper.process();
 			wrapper.save();
+
+			// Spend some time to simulate processing...
+	        try {
+	            Thread.sleep(20000);
+	        } catch (InterruptedException e) {
+	        }
+			
+			
 		} catch ( Exception e ) {
 			System.out.println("Wrapper execution error: " + e.getMessage() );
 		}
--- extra/examples/wrappers/benchmark/src/br/cefetrj/sagitarii/wrappers/Processor.java
+++ extra/examples/wrappers/benchmark/src/br/cefetrj/sagitarii/wrappers/Processor.java
@@ -41,20 +41,19 @@ public class Processor implements IWrapperProcessor {
 		
 		System.out.println("Source Data: " + attr01 + "/" + attr02 + "/" + attr03);
 		
-        try {
-            Thread.sleep(20000);
-        } catch (InterruptedException e) {
-        }
-		
 	}
 
 	
 	@Override
 	public void onProcessFinish() {
+		
+		System.out.println("done.");
+
+		
 		// Get the first data just to create the header. Can be anyone since 
 		// the header is present in all items
 		// In most cases this method can left untouched because it can
-		// be used by MAP (1:1), REDUCE (n:1) or SPLIT (1:n) operator.
+		// be used by MAP (1:1), REDUCE (n:1) or SPLIT (1:n) operators.
 		if ( csvLines.size() > 0 ) {
 			outputData.add( csvLines.get(0).getCsvHeader() );	
 			for ( LineData ld : csvLines ) {
@@ -68,7 +67,7 @@ public class Processor implements IWrapperProcessor {
 	
 	@Override
 	public void onProcessBeforeStart( LineData headerData ) {
-		//
+		System.out.println("starting...");
 	}
 
 
