d44dd3d9c8b6fc513388fb93dc53e1d2c72923ce
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2016-04-24 23:37:26 -0300

    Hadoop HDFS implementation [ Teapot ] Done.

---
+++ extra/upload/src/main/java/br/cefetrj/sagitarii/teapot/User.java
@@ -0,0 +1,68 @@
+package br.cefetrj.sagitarii.teapot;
+
+public class User {
+	private int idUser;
+	private String fullName;
+	private String userMail;
+	private String loginName;
+	private String details;
+	private String token;
+	private String password;
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getDetails() {
+		return details;
+	}
+	
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	
+	public String getToken() {
+		return token;
+	}
+	
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+	public String getUserMail() {
+		return userMail;
+	}
+
+	public void setUserMail(String userMail) {
+		this.userMail = userMail;
+	}
+
+}
